c582309c06604e439cc801f66cb1d4d4b530d7ab
arjunkapoor14422214-png <arjunkapoor14422214@gmail.com>
2025-11-13 10:09:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -137,5 +137,6 @@ def home():
 import threading
 threading.Thread(target=lambda: bot.infinity_polling(skip_pending=True, timeout=10)).start()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
